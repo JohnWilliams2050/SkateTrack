e1d1de7905293a97bc35bad40e027fec5d194e5e
Juan Felipe Rodriguez Amador <rodrigueza-jf@javeriana.edu.co>
2025-05-29 18:53:31 -0500

    Modificar variables de entorno

--- client/src/routes/ClubsPage.jsx
+++ client/src/routes/ClubsPage.jsx
@@ -18,7 +18,7 @@ const center = {
 
 const ClubsPage = () => {
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: 'API KEY'
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY
   });
 
   if (!isLoaded) return <div>Cargando mapa...</div>;
